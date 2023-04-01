54beaa08e8aa665d474913055b962a7035717a0b
eghazaryan <eghazaryan@aligntech.com>
2023-04-01 23:27:20 +0400

    change name

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:eghazaryan/fishing-card.git main:gh-pages
+git push -f git@github.com:eghazaryan/ui-exercise.git main:gh-pages
 
 cd -
